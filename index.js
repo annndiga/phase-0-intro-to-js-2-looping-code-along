3016663ac9d622adb066ea889666bd8fc96c3e77
Ann Ndiga <annndiga@gmail.com>
2023-06-21 15:03:29 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+ const messages = [];
+for (let i = 0; i < namesArray.length; i++) {
+    messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+}
+return messages;
+
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "birthday";
+const result = writeCards(names, eventName);
+console.log(result);
+
+function countDown(num) {
+for (let i = num; i >= 0; i--) {
+    console.log(i);
+}
+}
+
